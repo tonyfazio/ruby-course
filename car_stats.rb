c11fafa7a583fb77aa7994c5ac6b235852a34dc6
Tony Fazio <antoniojfazio@gmail.com>
2014-04-24 14:06:50 -0500

    finished car exercise

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,26 @@
+
 class CarStats
-  # TODO
+  @@cars_per_color= Hash.new
+  @@most_popular_color= nil
+  @@least_popular_color = nil
+
+  def self.calc_top_color(cars)
+    cars.each do |x|
+      if @@cars_per_color.include?(x.color)
+        @@cars_per_color[x.color]+=1
+      else
+        @@cars_per_color[x.color] = 1
+      end
+    end
+    popular= @@cars_per_color.max_by {|x,y| y}
+    @@most_popular_color= popular[0]
+    @@most_popular_color
+  end
+
+  def self.calc_bottom_color(cars)
+    lame = @@cars_per_color.min_by {|x,y| y}
+    @@least_popular_color = lame[0]
+    @@least_popular_color
+  end
 end
+
