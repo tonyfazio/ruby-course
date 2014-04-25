da10be31960bbf6cc778cbb4713b4513f6469392
Tony Fazio <antoniojfazio@gmail.com>
2014-04-25 14:52:52 -0500

    passed 6 tests

--- bar.rb
+++ bar.rb
@@ -1,4 +1,29 @@
 require 'time' # you're gonna need it
 
 class Bar
+
+  attr_reader :name
+  attr_accessor :menu_items, :happy_discount
+
+  def initialize(name)
+    @name = name
+    @menu_items =[]
+    @happy_discount = 0
+  end
+
+  def add_menu_item(item, price)
+    x =Item.new(item, price)
+    @menu_items<<x
+  end
+
+end
+
+class Item
+  attr_accessor :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
 end
