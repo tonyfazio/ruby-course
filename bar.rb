6a020215d761feaba8b66bc2cbb048cde6082325
Tony Fazio <antoniojfazio@gmail.com>
2014-04-25 16:42:12 -0500

    finished first set of extenetions but didn't write test

--- bar.rb
+++ bar.rb
@@ -3,12 +3,14 @@ require 'time' # you're gonna need it
 class Bar
 
   attr_reader :name
-  attr_accessor :menu_items, :happy_discount
+  attr_accessor :menu_items, :happy_discount, :total_sales, :purchase_list
 
   def initialize(name)
     @name = name
     @menu_items =[]
     @happy_discount = 0
+    @total_sales = 0
+    @purchase_list = {}
   end
 
   def add_menu_item(item, price)
@@ -16,9 +18,25 @@ class Bar
     @menu_items<<x
   end
 
+  def add_to_discount_list (item)
+    @menu_items.each do |x|
+      if x.name == name
+        x.discount = true
+      else
+        return
+      end
+    end
+  end
+
   def happy_discount
+    date = Time.now.wdate
+
     if happy_hour?
-      @happy_discount= 0.5
+      if date ==1 || date == 3
+        @happy_discount=0.5
+      else
+        @happy_discount=.25
+      end
     else
       @happy_discount = 0
     end
@@ -44,7 +62,7 @@ class Bar
     item_price = 0
     @menu_items.each do |x|
       if x.name == name
-        if happy_hour?
+        if happy_hour? && x.discount?
           item_price = x.price*@happy_discount
            return item_price
         else
@@ -57,14 +75,29 @@ class Bar
     end
   end
 
+  def purchase (name, quantity)
+    purchase_total = 0
+     @menu_items.each do |x|
+      if x.name == name
+        purchase_total = get_price(x.name)*quantity
+        purchase_list[x.name]+=purchase_total
+        @total_sales+=purchase_total
+        return purchase_total
+      else
+        puts "The bartender doesn't know how to make the drink"
+      end
+    end
+  end
+
 end
 
 class Item
-  attr_accessor :name, :price
+  attr_accessor :name, :price, :discount
 
-  def initialize(name, price)
+  def initialize(name, price, discount = false)
     @name = name
     @price = price
+    @discount = discount
   end
 
 end
