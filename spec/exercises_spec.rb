5d68d0b610076db1175dbd4f79e882f56126d040
Tony Fazio <antoniojfazio@gmail.com>
2014-04-28 14:35:31 -0500

    finished exercise 2

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -19,3 +19,10 @@ describe 'Exercise 1' do
     expect(result).to eq(4)
   end
 end
+
+describe 'Exercise 2' do
+  it " returns the second element in the array" do
+    result = Exercises.ex2([1,2,3,4])
+    expect(result).to eq(2)
+  end
+end
